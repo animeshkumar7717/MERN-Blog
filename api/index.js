b26deb530030090d52769241515a94b71acf207f
Animesh Kumar <animesh.kumar7717@gmail.com>
2024-08-06 13:18:02 +0530

    adding the user model

--- api/index.js
+++ api/index.js
@@ -2,7 +2,12 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/', (req,res)=>{
+    res.send('Api is working...')
+})
+
 app.listen(3000, ()=>{
     console.log(`Server is running on port 3000!`);
-    
 })
