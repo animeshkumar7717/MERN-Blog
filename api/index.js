bf8a1ca3a295cddba835f87618f639ac4ff43f84
Animesh Kumar <animesh.kumar7717@gmail.com>
2024-08-07 18:28:00 +0530

    create sign up page ui

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 
@@ -21,6 +22,7 @@ mongoose
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Api is working...");
